2e67d07da21c4c66442bec46b6af321f58530391
gitroll2023 <class2023.05@gmail.com>
2025-05-12 17:56:11 +0900

    2

--- src/app/programs/page.tsx
+++ src/app/programs/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
 import ImageProtection from '../../components/ImageProtection';
 
 export default function ProgramsPage() {
