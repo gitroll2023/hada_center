cf8f1c71df37d0d88808fc7462038d251ee3bc07
gitroll2023 <class2023.05@gmail.com>
2025-05-15 21:43:44 +0900

    메인페이지 인터페이스 수정

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { motion } from "framer-motion";
 
 const HeroSection = () => {
@@ -18,6 +19,21 @@ const HeroSection = () => {
 
   return (
     <section className="relative min-h-screen overflow-hidden bg-gradient-to-b from-indigo-900 via-purple-800 to-blue-700">
+      {/* 3층 공간 이미지 - 은은하게 표시 */}
+      <div className="absolute inset-0 overflow-hidden z-0">
+        <div className="absolute inset-0 opacity-15 mix-blend-overlay">
+          <div className="relative w-full h-full">
+            <Image
+              src="/images/space/3F_3.jpg"
+              alt="3층 공간"
+              fill
+              className="object-cover object-center"
+              priority
+            />
+          </div>
+        </div>
+      </div>
+        
       {/* 배경 장식 요소들 */}
       <div className="absolute inset-0 overflow-hidden">
         {/* 움직이는 그라데이션 원 */}
@@ -119,13 +135,23 @@ const HeroSection = () => {
         </motion.div>
       </div>
       
-      {/* 하단 웨이브 효과 */}
-      <div className="absolute bottom-0 left-0 right-0">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 120" className="w-full h-auto">
+      {/* 하단 웨이브 효과 - 반응형 웨이브 */}
+      <div className="absolute bottom-0 left-0 right-0 overflow-hidden">
+        {/* 모바일용 웨이브 (더 완만한 곱선) */}
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none" className="w-full block md:hidden" style={{ height: '80px' }}>
           <path 
             fill="#ffffff" 
             fillOpacity="1" 
-            d="M0,64L48,80C96,96,192,128,288,128C384,128,480,96,576,85.3C672,75,768,85,864,101.3C960,117,1056,139,1152,133.3C1248,128,1344,96,1392,80L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+            d="M0,192L60,197.3C120,203,240,213,360,202.7C480,192,600,160,720,165.3C840,171,960,213,1080,218.7C1200,224,1320,192,1380,176L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+          ></path>
+        </svg>
+        
+        {/* 태블릿/데스크톱용 웨이브 (더 뛰렷한 물결) */}
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none" className="w-full hidden md:block" style={{ height: '120px' }}>
+          <path 
+            fill="#ffffff" 
+            fillOpacity="1" 
+            d="M0,128L40,138.7C80,149,160,171,240,165.3C320,160,400,128,480,128C560,128,640,160,720,186.7C800,213,880,235,960,218.7C1040,203,1120,149,1200,133.3C1280,117,1360,139,1400,149.3L1440,160L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
           ></path>
         </svg>
       </div>
